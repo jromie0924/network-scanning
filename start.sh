a80436eb056233142fff7a5167a35359131bbeda
Jackson Romie <jromie0924@gmail.com>
2026-09-03 18:02:00 -0500

    fix bug in startup script

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd "$SCRIPT_DIR/.." || exit 1
+cd "$SCRIPT_DIR" || exit 1
 
 pipenv sync
 pipenv run python app.py
