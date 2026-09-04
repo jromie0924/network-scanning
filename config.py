be7ab1a40c68ffc2c8575c25e571cc845f645a25
Jackson Romie <jromie0924@gmail.com>
2026-09-04 14:34:36 -0500

    logging to INFO

--- config.py
+++ config.py
@@ -2,13 +2,13 @@ import logging
 import os
 
 APP_NAME = "network-scanner"
-LOGGING_LEVEL = logging.DEBUG
+LOGGING_LEVEL = logging.INFO
 LOG_FILE = "logs/network-scanner.log"
 
 AWS_SECRET = f'{os.environ.get("HOME", "/home/jackson")}/.aws_secret/network_scanner_app_accessKeys.csv'
 AWS_REGION = "us-east-2"
 
-SCANNING_INTERFACE = "eth0"
+SCANNING_INTERFACE = "en0"
 
 ARP_SCAN_SUDO = False
 ARP_SCAN_FREQUENCY = 2 # minutes
